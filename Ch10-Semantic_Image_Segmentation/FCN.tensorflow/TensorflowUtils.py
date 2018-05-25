0c58907a2a04990c1243ae6ae51db5bf4f69f029
SOLARIS <jinhoyang@snu.ac.kr>
2018-05-26 01:02:01 +0900

    utils update

--- Ch10-Semantic_Image_Segmentation/FCN.tensorflow/TensorflowUtils.py
+++ Ch10-Semantic_Image_Segmentation/FCN.tensorflow/TensorflowUtils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
+# 모델 구현을 위한 유틸리티 함수들
 
-# Utils used with tensorflow implemetation
 import tensorflow as tf
 import numpy as np
 import scipy.misc as misc
@@ -10,7 +10,7 @@ import tarfile
 import zipfile
 import scipy.io
 
-
+# VGGNet 파라미터가 저장된 mat 파일을 다운로드 받고 불러옵니다.
 def get_model_data(dir_path, model_url):
     maybe_download_and_extract(dir_path, model_url)
     filename = model_url.split("/")[-1]
@@ -43,36 +43,30 @@ def maybe_download_and_extract(dir_path, url_name, is_tarfile=False, is_zipfile=
                 zip_dir = zf.namelist()[0]
                 zf.extractall(dir_path)
 
-
+# 이미지를 png 파일로 저장합니다.
 def save_image(image, save_dir, name, mean=None):
     """
-    Save image by unprocessing if mean given else just save
-    :param mean:
-    :param image:
-    :param save_dir:
-    :param name:
-    :return:
+    만약 평균값을 argument로 받으면 평균값을 더한뒤에 이미지를 저장하고, 아니면 바로 이미지를 저장합니다.
     """
     if mean:
         image = unprocess_image(image, mean)
     misc.imsave(os.path.join(save_dir, name + ".png"), image)
 
-
+# 변수를 선언합니다.
 def get_variable(weights, name):
     init = tf.constant_initializer(weights, dtype=tf.float32)
-    var = tf.get_variable(name=name, initializer=init,  shape=weights.shape)
+    var = tf.get_variable(name=name, initializer=init, shape=weights.shape)
     return var
 
-
+# weight를 선언합니다.
 def weight_variable(shape, stddev=0.02, name=None):
-    # print(shape)
     initial = tf.truncated_normal(shape, stddev=stddev)
     if name is None:
         return tf.Variable(initial)
     else:
         return tf.get_variable(name, initializer=initial)
 
-
+# bias를 선언합니다.
 def bias_variable(shape, name=None):
     initial = tf.constant(0.0, shape=shape)
     if name is None:
@@ -80,22 +74,12 @@ def bias_variable(shape, name=None):
     else:
         return tf.get_variable(name, initializer=initial)
 
-
-def get_tensor_size(tensor):
-    from operator import mul
-    return reduce(mul, (d.value for d in tensor.get_shape()), 1)
-
-
+# Convolution을 정의합니다.
 def conv2d_basic(x, W, bias):
     conv = tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding="SAME")
     return tf.nn.bias_add(conv, bias)
 
-
-def conv2d_strided(x, W, b):
-    conv = tf.nn.conv2d(x, W, strides=[1, 2, 2, 1], padding="SAME")
-    return tf.nn.bias_add(conv, b)
-
-
+# Deconvolution(Transpose Convolution)을 정의합니다.
 def conv2d_transpose_strided(x, W, b, output_shape=None, stride = 2):
     # print x.get_shape()
     # print W.get_shape()
@@ -108,34 +92,18 @@ def conv2d_transpose_strided(x, W, b, output_shape=None, stride = 2):
     conv = tf.nn.conv2d_transpose(x, W, output_shape, strides=[1, stride, stride, 1], padding="SAME")
     return tf.nn.bias_add(conv, b)
 
-
-def leaky_relu(x, alpha=0.0, name=""):
-    return tf.maximum(alpha * x, x, name)
-
-
+# 2x2 max 풀링을 정의합니다.
 def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
 
-
+# 2x2 average 풀링을 정의합니다.
 def avg_pool_2x2(x):
     return tf.nn.avg_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
 
+# 이미지에 평균을 뺍니다.
 def process_image(image, mean_pixel):
     return image - mean_pixel
 
+# 이미지에 평균을 더합니다.
 def unprocess_image(image, mean_pixel):
     return image + mean_pixel
-
-def add_to_regularization_and_summary(var):
-    if var is not None:
-        tf.summary.histogram(var.op.name, var)
-        tf.add_to_collection("reg_loss", tf.nn.l2_loss(var))
-
-def add_activation_summary(var):
-    if var is not None:
-        tf.summary.histogram(var.op.name + "/activation", var)
-        tf.summary.scalar(var.op.name + "/sparsity", tf.nn.zero_fraction(var))
-
-def add_gradient_summary(grad, var):
-    if grad is not None:
-        tf.summary.histogram(var.op.name + "/gradient", grad)
